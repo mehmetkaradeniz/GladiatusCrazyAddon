56e45c2c3f1be6a85bbfb02331c672d67f680e6c
mehmetkaradeniz <mehmetkz61@gmail.com>
2020-07-09 20:59:17 +0300

    expedition

--- source/core/source/hotkey.js
+++ source/core/source/hotkey.js
@@ -49,7 +49,7 @@ var gca_hotkey = {
             this.eatBestFood();
         }
         else if (pageParams.mod == "location") {
-            this.attackExpedition(4);
+            this.attackExpedition(2);
         }
         else if (pageParams.mod == "dungeon") {
             if (this.shouldEnterDungeon()) {
